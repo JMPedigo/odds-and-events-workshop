3ebf6dfd2157100b6c7c89dac7b57b69032a010c
James Pedigo <ogidep.m.semaj@gmail.com>
2026-05-04 14:19:18 -0500

    add render function

--- index.js
+++ index.js
@@ -3,17 +3,17 @@
 /* 
 I need a number bank that should display all the numbers user has created.
 */
-const bank = [];
+const bank = [""];
 
 /*
 I need an even container.
 */
-const even = [];
+const even = [""];
 
 /*
 I need an odd container.
 */
-const odd = [];
+const odd = [""];
 
 // === Components ===
 
@@ -26,7 +26,7 @@ I need a "Sort 1" button to remove first number from number bank and add it to e
 
 I need a "Sort All" button to remove all the numbers from number bank, and add them to either the odd or even category.
 */
-function NumberBankForm() {
+function BankForm() {
   const $form = document.createElement("form");
 
   $form.innerHTML = `
@@ -43,4 +43,26 @@ function NumberBankForm() {
 Numbers are sorted into the correct category based on even or odd.
 */
 
+function AddOdd() {}
+
+function AddEven() {}
+
 // === Render ===
+
+/* need to initialize a render function
+ */
+function render() {
+  const $app = document.querySelector("#app");
+  $app.innerHTML = `
+        <h1>Odds & Events</h1>
+        <BankForm></BankForm>
+        <addNumberToBank></addNumberToBank>
+        <AddOdd></AddOdd>
+        <AddEven></AddEven>
+    `;
+
+  $app.querySelector("BankForm").replaceWith(BankForm());
+  $app.querySelector("AddOdd").replaceWith(AddOdd());
+  $app.querySelector("AddEven").replaceWith(AddEven());
+}
+render();
