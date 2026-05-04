52653f95f3305a8bc478c8ce69f18ba01c8cfece
James Pedigo <ogidep.m.semaj@gmail.com>
2026-05-04 13:45:27 -0500

    add state variables

---
+++ index.js
@@ -0,0 +1,34 @@
+// === State ===
+
+/* 
+I need a number bank that should display all the numbers user has created.
+*/
+const bank = [];
+
+/*
+I need an even container.
+*/
+const even = [];
+
+/*
+I need an odd container.
+*/
+const odd = [];
+
+// === Components ===
+
+/*
+I need a form that allows input in the form of a number:
+
+I need an "Add number" button to move number entered in form input to the number bank.
+
+I need a "Sort 1" button to remove first number from number bank and add it to either the odd or even category.
+
+I need a "Sort All" button to remove all the numbers from number bank, and add them to either the odd or even category.
+
+Numbers are sorted into the correct category based on even or odd.
+
+
+*/
+
+// === Render ===
